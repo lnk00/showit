74561c7b7fe7319c74d9b018c19f039d13fc1d92
Damien Dumontet <damien.dumontet@protonmail.com>
2024-12-31 16:17:12 -0300

    remove layouts

--- lib/showit_web/auth/auth_controller.ex
+++ lib/showit_web/auth/auth_controller.ex
@@ -2,10 +2,10 @@ defmodule ShowitWeb.AuthController do
   use ShowitWeb, :controller
 
   def register(conn, _params) do
-    render(conn, "register.html")
+    render(conn, "register.html", layout: false)
   end
 
   def login(conn, _params) do
-    render(conn, "login.html")
+    render(conn, "login.html", layout: false)
   end
 end
--- lib/showit_web/landing/landing_controller.ex
+++ lib/showit_web/landing/landing_controller.ex
@@ -2,6 +2,6 @@ defmodule ShowitWeb.LandingController do
   use ShowitWeb, :controller
 
   def index(conn, _params) do
-    render(conn, "index.html")
+    render(conn, "index.html", layout: false)
   end
 end
